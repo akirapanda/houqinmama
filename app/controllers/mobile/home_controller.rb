69e7890cb12058c71fd0943dc9bba078bf811338
Akira Panda <p.chenliang@gmail.com>
2014-05-22 22:20:36 +0800

    add

--- app/controllers/mobile/home_controller.rb
+++ app/controllers/mobile/home_controller.rb
@@ -1,7 +1,7 @@
 class Mobile::HomeController < Mobile::BaseController
   def index
     @products = Product.on_sale
-    if params[:open_id].present
+    if params[:open_id].present?
       session[:open_id] = params[:open_id]
     end
   end
