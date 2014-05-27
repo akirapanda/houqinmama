a3948db017ca2dce264554180fa42ac85d1f12c8
Akira Panda <p.chenliang@gmail.com>
2014-05-27 21:41:07 +0800

    diaocha wenjuan test

--- app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
+++ app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
@@ -18,7 +18,9 @@ WeixinRailsMiddleware::WeixinController.class_eval do
     def response_text_message(options={})
       if @keyword == "test"
         link = "<a href='#{server_path}/mobile?open_id=#{@weixin_message.FromUserName}'> 商城入口</a>"
-        logger.info "log is #{link}"
+        reply_text_message(link)
+      elsif @keyword == "diaocha"
+        link = "<a href='http://jinshuju.net/f/fTYCcx'>客户满意度调查表</a>"
         reply_text_message(link)
       else
         reply_text_message("Your Message: #{@keyword}")
