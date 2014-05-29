9d3e186df6b015ef7edea3b5014e0fb03bb66462
Akira Panda <p.chenliang@gmail.com>
2014-05-29 22:36:36 +0800

    change weichat

--- app/controllers/mobile/base_controller.rb
+++ app/controllers/mobile/base_controller.rb
@@ -1,6 +1,11 @@
 class Mobile::BaseController < ApplicationController
   layout "mobile"
+  before_filter ,:get_weichat_id
   
-
+  def get_weichat_id
+    if params[:weichat_id].present?
+      session[:open_id] = params[:weichat_id]
+    end
+  end
   
 end
--- app/controllers/mobile/home_controller.rb
+++ app/controllers/mobile/home_controller.rb
@@ -1,9 +1,7 @@
 class Mobile::HomeController < Mobile::BaseController
   def index
     @products = Product.on_sale.order("no asc")
-    if params[:weichat_id].present?
-      session[:open_id] = params[:weichat_id]
-    end
+
   end
   
   def category
